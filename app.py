d16578e71574ab32b16dda69fa7971fa48c39305
Panji Lenggari <panjilenggari@yandex.ru>
2020-05-28 04:42:53 +0700

    new files added

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for #import module Flask Framwork dan render template dari html atau jinja
 
-app = Flask(__name__)
+app = Flask(__name__) #berfungsi untuk memanggil App File
 
-@app.route('/')
-def home():
-    return render_template('home.jinja')
+@app.route('/') #python decorator di dalam funtion untuk memberitahu browser bahwa ini page pertama yang di reques
+def home(): #funtion
+    return render_template('home.jinja') #asumsikan file home.jinja ini adalah file home.html 
 
-if __name__=='__main__':
-    app.run(debug=True)
+@app.route('/flask')
+def flask():
+    return render_template('flask.jinja')
+
+if __name__=='__main__': #harus di definisikan supaya source code nya run
+    app.run(debug=True) #debug True untuk mempermudah jika ada error dan bisa di perbaiki langsung di browser
